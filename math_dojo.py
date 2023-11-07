1107bc3d58c623b7d7b5468ed4ed7d73f2a9aec9
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-07 15:23:52 -0600

    I created a class Mathdojo with 1 attribute: result

---
+++ math_dojo.py
@@ -0,0 +1,4 @@
+# Create a Python class called MathDojo that has one attribute, result, and 2 methods: add and subtract. The 2 methods each must take at least 1 parameter, but could take many more.
+class Mathdojo:
+    def __init__(self, result):
+        self.result = result
