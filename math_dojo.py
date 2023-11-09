115fb5e23c9c0784e3039043c7ac78ffb62bd12e
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-08 21:13:11 -0600

    I added add() and subtract()

--- math_dojo.py
+++ math_dojo.py
@@ -1,4 +1,20 @@
-# Create a Python class called MathDojo that has one attribute, result, and 2 methods: add and subtract. The 2 methods each must take at least 1 parameter, but could take many more.
+# Create a Python class called MathDojo that has one attribute result
 class Mathdojo:
-    def __init__(self, result):
-        self.result = result
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num, *nums):
+        self.result += num
+        for i in nums:
+            self.result += i
+        return self
+    
+    def subtract(self, num, *nums):
+        self.result -= num
+        for i in nums:
+            self.result -= i
+        return self
+    
+resul_1 = Mathdojo()
+x = resul_1.add(2,3,5,6).subtract(2,9,3,1).result
+print(x)
